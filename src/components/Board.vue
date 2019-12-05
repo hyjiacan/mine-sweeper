dddac5d52d6ef3ee063828e7ba81b58c28ef9df7
hyjiacan <hyjiacan@163.com>
2019-12-05 16:03:58 +0800

    添加 Board组件及其数据声明与表格展示

---
+++ src/components/Board.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="board">
+    <table>
+      <tr v-for="i in rows" :key="i">
+        <td v-for="j in cols" :key="j">{{i}}-{{j}}</td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Board",
+  data() {
+    return {
+      // 使用 data 作为扫雷的数据存储
+      data: [],
+      level: 1,
+      mineCount: 9,
+      rows: 9,
+      cols: 9
+    };
+  }
+};
+</script>
